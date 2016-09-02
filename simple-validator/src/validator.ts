dd10e7c28df361a65bb9498aa60980bf11547719
pcan <pierocangianiello@gmail.com>
2016-09-02 12:18:15 +0200

    fixed null check on object[member.name].

--- simple-validator/src/validator.ts
+++ simple-validator/src/validator.ts
@@ -4,11 +4,11 @@ export function isValid(object: any, interfaceObj: Interface): boolean {
 
     if (interfaceObj.members) { // members may be undefined if there are no members.
         for (let member of interfaceObj.members) {
-            if (isPrimitive(member.type) && object[member.name] != null && member.type.kind != typeof object[member.name]) {
+            if (isPrimitive(member.type) && object[member.name] && member.type.kind != typeof object[member.name]) {
                 // found a primitive type field in the interface, but not in the object
                 console.log(`Field ${member.name} should be ${member.type.kind} but it is ${typeof object[member.name]}`)
                 return false;
-            } else if (member.type.kind === 'interface' && object[member.name] != null) {
+            } else if (member.type.kind === 'interface' && object[member.name]) {
                 // recursively check field type against the corresponding interface
                 if(!isValid(object[member.name], <Interface>member.type)) {
                     return false;
